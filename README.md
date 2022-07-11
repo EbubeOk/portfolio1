fb640fce5fe7315604802763693b9dd0fff1a3cd
EbubeOk <72478331+EbubeOk@users.noreply.github.com>
2022-07-11 19:52:13 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Setup:
+
+run npm i && npm run start to start the development server
---
+++ src/data.js
@@ -0,0 +1,128 @@
+// Skills icons - https://react-icons.github.io/react-icons/
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaBootstrap,
+  FaReact,
+  FaGitAlt,
+  FaGithubSquare,
+  FaLink,
+} from "react-icons/fa";
+import { SiJavascript, SiJava, SiPython, SiCplusplus } from "react-icons/si";
+
+// Projects Images (add your images to the images directory and import below)
+import Logo from "./images/logo.svg";
+
+// Hero Images (add your images to the /images directory with the same names)
+import HeroLight from "./images/hero-light.jpg";
+import HeroDark from "./images/hero-dark.jpg";
+// If you change the import names above then you need to change the export names below
+export { HeroLight as Light };
+export { HeroDark as Dark };
+
+/* START HERE
+ **************************************************************
+  Add your GitHub username (string - "YourUsername") below.
+*/
+export const githubUsername = "EbubeOk";
+
+// Blog link icon (imported above)
+export const Blog = <FaLink />;
+
+/* About Me
+ **************************************************************
+  Add a second paragraph for the about me section.
+*/
+export const moreInfo =
+  "An innovative software engineer who is passionate about technology, enjoys learning more about it and helping others use it to improve their lives and be more productive. Welcome to a showcase of my projects and abilities.";
+//I built this site with React, Bootstrap, Redux, and the GitHub REST API
+
+/* Skills
+ ************************************************************** 
+  Add or remove skills in the SAME format below, there must be one icon imported above per skill below.
+*/
+export const skillData = [
+  {
+    id: 1,
+    skill: <FaHtml5 className="display-4" />,
+    name: "HTML5",
+  },
+  {
+    id: 2,
+    skill: <FaCss3Alt className="display-4" />,
+    name: "CSS3",
+  },
+  {
+    id: 3,
+    skill: <SiJavascript className="display-4" />,
+    name: "JavaScript",
+  },
+  {
+    id: 4,
+    skill: <FaBootstrap className="display-4" />,
+    name: "BootStrap",
+  },
+  {
+    id: 5,
+    skill: <FaReact className="display-4" />,
+    name: "React",
+  },
+  {
+    id: 6,
+    skill: <SiJava className="display-4" />,
+    name: "Java",
+  },
+  {
+    id: 7,
+    skill: <SiCplusplus className="display-4" />,
+    name: "C++",
+  },
+  {
+    id: 8,
+    skill: <FaGitAlt className="display-4" />,
+    name: "Git",
+  },
+  {
+    id: 9,
+    skill: <FaGithubSquare className="display-4" />,
+    name: "GitHub",
+  },
+  {
+    id: 10,
+    skill: < SiPython className="display-4" />,
+    name: "Python",
+  },
+];
+
+// Resume link (string - "https://YourResumeUrl") - I am using CloudFront to share my resume (https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/Introduction.html)
+export const resume = null;
+
+/* Projects
+ ************************************************************** 
+  List the repo names (string - "your-repo-name") you want to include (they will be sorted alphabetically). If empty, only the first 3 will be included.
+*/
+export const filteredProjects = ["googleclone", "twitchclone", "realtor-realestate"];
+
+// Replace the defualt GitHub image for matching repos below (images imported above - lines 13-14)
+export const projectCardImages = [
+  {
+    name: "gooleclone",
+    // image: Logo,
+  },
+  {
+    name: "twitchclone",
+    // image: Logo,
+  },
+  {
+    name: "realtor-realestate",
+    // image: Logo,
+  },
+
+];
+
+/* Contact Info
+ ************************************************************** 
+  Add your formspree endpoint below.
+  https://formspree.io/
+*/
+export const formspreeUrl = "https://formspree.io/f/YourEndpoint";
